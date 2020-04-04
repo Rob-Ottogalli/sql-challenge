9e64a38477beeb33e822f55f0bf4dad305b585d1
Robert Ottogalli <59754310+Rob-Ottogalli@users.noreply.github.com>
2020-04-04 17:19:00 -0500

    Completed query challenge

--- EmployeeSQL/Query.sql
+++ EmployeeSQL/Query.sql
@@ -51,3 +51,39 @@ SELECT d.dept_no,
    FROM "Employees" as e
   WHERE first_name = 'Hercules'
     AND last_name LIKE 'B%';
+	
+-- 6. List all employees in the Sales department, including their:
+-- employee number, last name, first name, and department name.
+
+ SELECT e.emp_no,
+		e.last_name,
+		e.first_name,
+		d.dept_name
+   FROM "Employees" as e
+   JOIN "Department_Employee" as de
+     ON e.emp_no = de.emp_no
+   JOIN "Departments" as d
+	 ON de.dept_no = d.dept_no
+  WHERE d.dept_name = 'Sales';
+
+-- 7. List all employees in the Sales and Development departments, including their:
+-- employee number, last name, first name, and department name.
+
+ SELECT e.emp_no,
+		e.last_name,
+		e.first_name,
+		d.dept_name
+   FROM "Employees" as e
+   JOIN "Department_Employee" as de
+     ON e.emp_no = de.emp_no
+   JOIN "Departments" as d
+	 ON de.dept_no = d.dept_no
+  WHERE d.dept_name = 'Sales' or d.dept_name = 'Development';
+
+-- 8. In descending order, list the frequency count of employee last names, i.e.,
+-- how many employees share each last name
+
+SELECT last_name, COUNT(last_name) as "last_name_frequency"
+  FROM "Employees" as e
+ GROUP BY last_name
+ ORDER BY "last_name_frequency" DESC;
